26a82a32331defdc58ba7c12e705b9302026e079
ManshulDuggal <manshuld7@gmail.com>
2023-09-30 19:27:45 +0900

    all commmits

---
+++ lib/arraydummy/NavLinks.tsx
@@ -0,0 +1,9 @@
+const NavLinks = [
+  { head: "Home", link: "#" },
+  { head: "Projects", link: "#Projects" },
+  { head: "Contact", link: "#Contact" },
+  { head: "Skills", link: "#Skills" },
+  //   { head: "", link: "#Skills" },
+];
+
+export default NavLinks
