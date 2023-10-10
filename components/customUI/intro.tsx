a90b9c718232ca5cce8a8972d838cf40965660de
ManshulDuggal <manshuld7@gmail.com>
2023-10-10 17:37:03 +0900

    more changes to ui

--- components/customUI/intro.tsx
+++ components/customUI/intro.tsx
@@ -10,56 +10,58 @@ const Intro = () => {
 
   return (
     <>
-      {language === "en" ? (
-        <section className="flex flex-col items-center justify-center text-center space-y-5 lg:h-screen h-screen lg:font-light ">
-          <div className="max-w-xl ">
-            <h1>
-              As a software engineer, I excel at quickly learning and
-              implementing new technologies and concepts. I have a proven track
-              record of reducing team stress, introducing innovative ideas, and
-              improving existing projects. My collaborative approach helps
-              create a more efficient and harmonious work environment while
-              delivering high-quality results
-            </h1>
-          </div>
-          <div className="flex space-x-20 items-center justify-center ">
-            <Button className="h-auto w-auto text-xs lg:text-base">
-              <a href="ResumeEN.pdf" download>
-                Download CV
-              </a>
-              <ArrowDownIcon
-                href="/Docs/ResumeEN"
-                className=" animate animate-pulse lg:w-5 h-auto text-xs lg:text-base w-max "
-              />{" "}
-            </Button>
-            <Button>
-              <Link href={"#Contact"}> Contact me here</Link>
-            </Button>
-          </div>
-        </section>
-      ) : (
-        <section className="flex flex-col items-center justify-center text-center space-y-5 lg:h-screen h-screen lg:font-light ">
-          <div className="max-w-xl ">
-            <h1>
-              ソフトウェアエンジニアとして、新しい技術やコンセプトを迅速に学習し、実装する能力に優れています。私はチームのストレスを軽減し、革新的なアイデアを導入し、既存のプロジェクトを改善することで、実績を重ねてきました。また、協力的なアプローチを取ることで、効率的で調和の取れた職場環境を促進し、高品質な結果を提供するのに貢献しています。
-            </h1>
-          </div>
-          <div className="flex space-x-20 items-center justify-center ">
-            <Button className="h-auto w-auto text-xs lg:text-base">
-              <a href="ResumeEN.pdf" download>
-                履歴書をダウンロード
-              </a>
-              <ArrowDownIcon
-                href="/Docs/ResumeEN"
-                className=" animate animate-pulse lg:w-5 h-auto text-xs lg:text-base w-max "
-              />{" "}
-            </Button>
-            <Button>
-              <Link href={"#Contact"}> お問い合わせください</Link>
-            </Button>
-          </div>
-        </section>
-      )}
+      <div className="dark:bg-gradient-to-b bg-gradient-to-b  dark:from-stone-700   dark:to-slate-black  lg:p-24 w-full  ">
+        {language === "en" ? (
+          <section className="flex flex-col items-center justify-center text-center space-y-5 lg:h-screen h-screen lg:font-light     ">
+            <div className="max-w-xl ">
+              <h1>
+                As a software engineer, I excel at quickly learning and
+                implementing new technologies and concepts. I have a proven
+                track record of reducing team stress, introducing innovative
+                ideas, and improving existing projects. My collaborative
+                approach helps create a more efficient and harmonious work
+                environment while delivering high-quality results
+              </h1>
+            </div>
+            <div className="flex space-x-20 items-center justify-center ">
+              <Button className="h-auto w-auto text-xs lg:text-base">
+                <a href="ResumeEN.pdf" download>
+                  Download CV
+                </a>
+                <ArrowDownIcon
+                  href="/Docs/ResumeEN"
+                  className=" animate animate-pulse lg:w-5 h-auto text-xs lg:text-base w-max "
+                />{" "}
+              </Button>
+              <Button>
+                <Link href={"#Contact"}> Contact me here</Link>
+              </Button>
+            </div>
+          </section>
+        ) : (
+          <section className="flex flex-col items-center justify-center text-center space-y-5 lg:h-screen h-screen lg:font-light ">
+            <div className="max-w-xl ">
+              <h1>
+                ソフトウェアエンジニアとして、新しい技術やコンセプトを迅速に学習し、実装する能力に優れています。私はチームのストレスを軽減し、革新的なアイデアを導入し、既存のプロジェクトを改善することで、実績を重ねてきました。また、協力的なアプローチを取ることで、効率的で調和の取れた職場環境を促進し、高品質な結果を提供するのに貢献しています。
+              </h1>
+            </div>
+            <div className="flex space-x-20 items-center justify-center ">
+              <Button className="h-auto w-auto text-xs lg:text-base">
+                <a href="ResumeEN.pdf" download>
+                  履歴書をダウンロード
+                </a>
+                <ArrowDownIcon
+                  href="/Docs/ResumeEN"
+                  className=" animate animate-pulse lg:w-5 h-auto text-xs lg:text-base w-max "
+                />{" "}
+              </Button>
+              <Button>
+                <Link href={"#Contact"}> お問い合わせください</Link>
+              </Button>
+            </div>
+          </section>
+        )}
+      </div>
     </>
   );
 };
--- components/wrapper/Wrap.tsx
+++ components/wrapper/Wrap.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Navbar from "../customUI/navbar";
 import Intro from "../customUI/intro";
-import ProjectsCards from "../customUI/projects";
+
 import Divide from "../customUI/divide";
 import About from "../customUI/about";
 import Skills from "../customUI/skills";
@@ -9,19 +9,17 @@ import Contact from "../customUI/contact";
 import Mobres from "../customUI/mobres";
 import Projects from "../customUI/projects";
 
-interface Props {}
-
 const Wrap = () => {
   return (
     <div>
-      <main className="   lg:pb-20  flex wrap flex-col items-center justify-between  ">
+      <main className="   lg:pb-20  flex wrap flex-col items-center justify-between ">
         <div className="z-50 pt-5  fixed">
           <Navbar />
         </div>
 
         <Mobres />
 
-        <div className="!w-11/12  m-auto flex flex-col justify-center items-center lg:space-y-28 space-y-10">
+        <div className="!w-10/12  m-auto flex flex-col justify-center items-center lg:space-y-28 space-y-10    overflow-hidden ">
           <Intro />
           <Divide />
           <Projects />
@@ -32,12 +30,12 @@ const Wrap = () => {
           <Divide />
           <Contact />
         </div>
-        <div className="absolute top[90rem] right-0 rounded-full antialiased bg-green-200 opacity-70 dark:bg-pink-500 blur-[20rem] w-[31.25rem] h-[31.25rem] z-[-100]">
+        {/* <div className="absolute top[90rem] right-0 rounded-full antialiased bg-green-200 opacity-70 dark:bg-pink-500 blur-[20rem] w-[31.25rem] h-[31.25rem] z-[-100]">
           {" "}
         </div>
         <div className="absolute -top[-6rem] -left-52  rounded-full antialiased bg-purple-200 opacity-100 dark:bg-pink-500 blur-[20rem] w-[31.25rem] h-[31.25rem] z-[-100] ">
           {" "}
-        </div>
+        </div> */}
       </main>
     </div>
   );
